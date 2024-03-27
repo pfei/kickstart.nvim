50349261c2bd32eef42108c95bf7c6a231464aa8
Pierre Feilles <pierre.feilles@gmail.com>
2024-03-27 14:06:09 +0100

    Add catpuccin/nvim

--- init.lua
+++ init.lua
@@ -747,6 +747,8 @@ require('lazy').setup({
     end,
   },
 
+  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
